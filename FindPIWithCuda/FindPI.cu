5acf5c297e3fe183279364461229f69488aef83f
donchak <vyacheslav.donchak@devexpress.com>
2019-04-15 17:45:58 +0300

    Add project files.

---
+++ FindPIWithCuda/FindPI.cu
@@ -0,0 +1,145 @@
+// System includes
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+
+// CUDA runtime
+#include <cuda_runtime.h>
+
+// Helper functions and utilities to work with CUDA
+#include <helper_functions.h>
+#include <helper_cuda.h>
+
+#include "Common.h"
+
+__global__ void preparePoints(const TPointData *input, TPointData *output, int count)
+{
+	int i = blockDim.x * blockIdx.x + threadIdx.x;
+
+	if (i < count)
+	{
+		auto supportPoint = input[i];
+		auto nextSupportPoint = input[(i + 1) % count];
+		double nextPointX = (supportPoint.X + nextSupportPoint.X) / 2;
+		double nextPointY = (supportPoint.Y + nextSupportPoint.Y) / 2;
+		double radiusToNextPoint = sqrt(nextPointX * nextPointX + nextPointY * nextPointY);
+		TPointData nextPoint;
+		nextPoint.X = nextPointX / radiusToNextPoint;
+		nextPoint.Y = nextPointY / radiusToNextPoint;
+		output[i * 2] = supportPoint;
+		output[(i * 2) + 1] = nextPoint;
+	}
+}
+
+__global__ void calcLength(const TPointData *input, int count, double* accumulator)
+{
+	__shared__ double local_lengths[256];
+	int i = blockDim.x * blockIdx.x + threadIdx.x;
+	if (i < count)
+	{
+		auto prevPoint = input[i];
+		auto point = input[(i + 1) % count];
+		double diffX = point.X - prevPoint.X;
+		double diffY = point.Y - prevPoint.Y;
+		double length = sqrt(diffX * diffX + diffY * diffY);
+		local_lengths[threadIdx.x] = length;
+	}
+	__syncthreads();
+
+	if (threadIdx.x == 0) {
+		double local_sum = 0;
+		for (int j = i, int local_index = 0; i < count; i++, local_index++) {
+			local_sum += local_lengths[local_index];
+		}
+		accumulator[blockIdx.x] = local_sum;
+	}
+}
+
+/**
+ * Program main
+ */
+int main(int argc, char **argv) {
+    printf("[Finding PI Using CUDA] - Starting...\n");
+
+	int i_count = 4;
+	size_t i_size = i_count * sizeof(TPointData);
+	TPointData *h_V = (TPointData *)malloc(i_size);
+
+	h_V[0].X = 1;
+	h_V[0].Y = 0;
+	h_V[1].X = 0;
+	h_V[1].Y = -1;
+	h_V[2].X = -1;
+	h_V[2].Y = 0;
+	h_V[3].X = 0;
+	h_V[3].Y = 1;
+
+	// Allocate the device input vector
+	TPointData *d_iV = NULL;
+	checkCudaErrors(cudaMalloc((void **)&d_iV, i_size));
+
+	// Copy the host input vectors in host memory to the device input vectors in
+	// device memory
+	printf("Copy input data from the host memory to the CUDA device\n");
+	checkCudaErrors(cudaMemcpy(d_iV, h_V, i_size, cudaMemcpyHostToDevice));
+
+
+	for (int i = 0; i < 10; i++) {
+
+		TPointData * d_oV = NULL;
+		checkCudaErrors(cudaMalloc((void **)&d_oV, i_size  * 2));
+
+		int threadsPerBlock = i_count < 256 ? i_count : 256;
+		int blocksPerGrid = (i_count + threadsPerBlock - 1) / threadsPerBlock;
+		printf("CUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
+
+		preparePoints<<<blocksPerGrid, threadsPerBlock>>>(d_iV, d_oV, i_count);
+
+		cudaDeviceSynchronize();
+
+		checkCudaErrors(cudaGetLastError());
+
+		checkCudaErrors(cudaFree(d_iV));
+
+		d_iV = d_oV;
+
+		i_count = i_count * 2;
+		i_size = i_size * 2;
+
+		threadsPerBlock = (i_count * 2) < 256 ? i_count : 256;
+		blocksPerGrid = (i_count + threadsPerBlock - 1) / threadsPerBlock;
+		printf("CUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
+
+
+		double result = 0;
+		double *h_accumelator = NULL;
+
+		h_accumelator = (double *)malloc(sizeof(double) * blocksPerGrid);
+
+		double *d_accumulator = NULL;
+		checkCudaErrors(cudaMalloc((void **)&d_accumulator, sizeof(double) * blocksPerGrid));
+		checkCudaErrors(cudaMemset(d_accumulator, 0, sizeof(double) * blocksPerGrid));
+
+		calcLength<<<blocksPerGrid, threadsPerBlock>>>(d_iV, i_count, d_accumulator);
+		cudaDeviceSynchronize();
+		checkCudaErrors(cudaGetLastError());
+
+		checkCudaErrors(cudaMemcpy(h_accumelator, d_accumulator, sizeof(double), cudaMemcpyDeviceToHost));
+
+		for (int j = 0; j < blocksPerGrid; j++) {
+			result += h_accumelator[j];
+		}
+
+		checkCudaErrors(cudaFree(d_accumulator));
+		free(h_accumelator);
+
+		fprintf(stdout, "Result is %f\n", result / 2);
+	}
+
+	// Free device global memory
+
+	checkCudaErrors(cudaFree(d_iV));
+
+	free(h_V);
+}
+
